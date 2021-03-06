7419a8187009c3727b309153661340df97d2cd28
Manuel Castañón-Puga <puga@uabc.edu.mx>
2021-03-05 16:24:57 -0800

    Version de manuel 2

--- src/uabc/edu/mx/Main.java
+++ src/uabc/edu/mx/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hola mundo!");
         System.out.println("Hola mundo cruel!");
+        System.out.println("bye!");
 	// write your code here
     }
 }
